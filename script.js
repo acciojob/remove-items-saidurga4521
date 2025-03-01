6303b623263f2530d0cb9525e3bba60bde814eb4
sai kamireddi <111673552+saidurga4521@users.noreply.github.com>
2025-03-01 11:07:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btnE1=document.getElementById('btn')
+const selectE1=document.getElementById('colorSelect')
+btnE1.addEventListener('click',()=>{
+	let val=selectE1.selectedIndex;
+	if(val>=0){
+	selectE1.remove(val);
+	}
+})
